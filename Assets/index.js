cef9da8c667b2bc3d7ce800d287d2942cfe6b526
seanianking <seanianking@gmail.com>
2020-02-24 18:35:25 -0700

    finished wizard functions

--- Assets/index.js
+++ Assets/index.js
@@ -2,10 +2,18 @@ var ElWiz = $("#wizard");
 
 
 
-$("#wizard").click(function(){
-$(this).toggleClass("hide")
+$("#wizDiv").click(function(){
 
+if ($("#wizard").is(":visible"))
+    $("#wizard").fadeOut(1000);
+else 
+    $("#wizard").fadeIn(1000);
 });
-$("#wizard").mouseEnter(function(){
-    $(this).append($("<span> Say hi!</span>"))
+
+$("#wizDiv").mouseenter(function(){
+    $(this).append($("<span id='greeting'> Say hi!</span>"))
+});
+
+$('#wizDiv').mouseleave(function(){
+    $('#greeting').remove()
 })
