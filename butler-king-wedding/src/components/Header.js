7ef152813ca53c040fe0f0e8400a56f1cfd27a7a
seanianking <seanianking@gmail.com>
2020-06-30 22:36:06 -0600

    working on adding images to header

--- butler-king-wedding/src/components/Header.js
+++ butler-king-wedding/src/components/Header.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
+import Daisy from '../assets/images/daisy.jpg'
 
 function Header() {
     return (
-        <div className="pt-3 pb-3">
+        <div className="pt-4 pb-3">
             <Container fluid>
                 <Row>
                     <Col>
+                    <Image fluid src={{ Daisy }}></Image>
+                    </Col>
+                    <Col>
                         <h1 className="headText">Caden and Hayleigh</h1>
+                    </Col>
+                    <Col>
+                    <Image fluid src={{ Daisy }}></Image>
                     </Col>
                 </Row>
             </Container>
